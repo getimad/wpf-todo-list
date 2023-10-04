b3d936a69e30fe3aaa33025ac96cd36ee5eaa73d
Imad Ez-zahi <getimad.pro@outlook.com>
2023-10-04 20:27:59 +0100

    Add Helper class to get connection string from App.config

---
+++ Utilities/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace TodoList.Models
+{
+    public static class Helper
+    {
+        public static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
