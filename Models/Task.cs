d932e389e99529cee2f4af465ccd5714d8010c1b
Imad Ez-zahi <getimad.pro@outlook.com>
2023-10-04 20:29:33 +0100

    Add Task model.

---
+++ Models/Task.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TodoList.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
