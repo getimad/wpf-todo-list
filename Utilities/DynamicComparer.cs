16faa8b5aa33e6fdf4b4cee552b3a2a398f890fc
Imad Ez-zahi <getimad.pro@outlook.com>
2023-11-09 22:13:31 +0100

    Add DynamicComparer class to Utilities directory
    - This class is like a helper class for comparing data by a specific property name.

---
+++ Utilities/DynamicComparer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace TodoList.Utilities
+{
+    internal class DynamicComparer<T> : IComparer<T>
+    {
+        private string _propertyName;
+
+        public DynamicComparer(string propertyName)
+        {
+            _propertyName = propertyName;
+        }
+
+        public int Compare(T x, T y)
+        {
+            var property = typeof(T).GetProperty(_propertyName);
+
+            var valueX = property.GetValue(x);
+            var valueY = property.GetValue(y);
+
+            return Comparer<object>.Default.Compare(valueX, valueY);
+        }
+    }
+}
